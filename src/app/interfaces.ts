13e7eea1d02299282d3737719d2a7113fefcdfce
Isabelle Kasas <isabelle.kasas@hotmail.fr>
2020-03-19 15:25:18 +0100

    fin tarif et gites

---
+++ src/app/interfaces.ts
@@ -0,0 +1,19 @@
+export interface PetitGroupe {
+    nuits: string;
+    euros: string;
+};
+export interface Groupe {
+    nuits: string;
+    euros: string;
+};
+export interface Semaine {
+    repas: string;
+    age: string;
+    euros: string;
+}
+export interface Rando {
+
+    forfait: string;
+    age: string;
+    euros: string;
+};
